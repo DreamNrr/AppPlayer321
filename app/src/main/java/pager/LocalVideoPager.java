8fe6da165827902ca1e80c00818f6c0598cd4c1e
DreamNrr <wzh74371233@163.com>
2017-05-19 16:55:42 +0800

    加载本地视频

--- app/src/main/java/pager/LocalVideoPager.java
+++ app/src/main/java/pager/LocalVideoPager.java
@@ -1,35 +1,103 @@
 package pager;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.wzh.appplayer321.R;
+
+import java.util.ArrayList;
+
+import adapter.LocalVideoAdapter;
+import domain.MediaItem;
 import fragment.BaseFragment;
 
-/**
- * 作者：杨光福 on 2017/5/19 11:47
- * QQ：541433511
- * 微信：yangguangfu520
- * 作用：
- */
+
 
 public class LocalVideoPager extends BaseFragment {
-    private TextView textView;
+    private ListView lv;
+    private TextView tv_nodata;
+    private LocalVideoAdapter adapter;
+
+    private ArrayList<MediaItem> mediaItems;
 
     //重写视图
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_local_video_pager,null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        //设置item的点击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //得到点击item对应的对象
+//                MediaItem mediaItem = mediaItems.get(position);
+
+                MediaItem item = adapter.getItem(position);
+                Toast.makeText(context, ""+item.toString(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+       return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("本地视频的内容");
+       getData();
+    }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(mediaItems != null && mediaItems.size() >0){
+                //有数据
+                tv_nodata.setVisibility(View.GONE);
+                //设置适配器
+                adapter = new LocalVideoAdapter(context,mediaItems);
+                lv.setAdapter(adapter);
+            }else{
+                //没有数据
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+    private void getData() {
+        new Thread(){
+            public void run(){
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,//视频在sdcard上的名称
+                        MediaStore.Video.Media.DURATION,//视频时长
+                        MediaStore.Video.Media.SIZE,//视频文件的大小
+                        MediaStore.Video.Media.DATA//视频播放地址
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if(cursor != null) {
+                    while(cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                        mediaItems.add(new MediaItem(name,duration,size,data));
+                        handler.sendEmptyMessage(0);
+                    }
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
