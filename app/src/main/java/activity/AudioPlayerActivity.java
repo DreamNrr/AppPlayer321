d37e21cc31cf0d539830d8dd94f816055f7e9324
DreamNrr <wzh74371233@163.com>
2017-05-24 19:08:22 +0800

    音乐播放器布局，动画

--- app/src/main/java/activity/AudioPlayerActivity.java
+++ app/src/main/java/activity/AudioPlayerActivity.java
@@ -1,15 +1,21 @@
 package activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.wzh.appplayer321.R;
 
 public class AudioPlayerActivity extends AppCompatActivity {
-
+    private ImageView iv_icon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
+        iv_icon = (ImageView)findViewById(R.id.iv_icon);
+        iv_icon.setBackgroundResource(R.drawable.animation_bg);
+        AnimationDrawable background = (AnimationDrawable) iv_icon.getBackground();
+        background.start();
     }
 }
