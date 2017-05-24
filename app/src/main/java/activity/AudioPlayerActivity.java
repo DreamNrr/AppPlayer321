189906603d5effa08360794436365b6ea95b646f
DreamNrr <wzh74371233@163.com>
2017-05-24 20:58:14 +0800

    播放音乐与暂停音乐

--- app/src/main/java/activity/AudioPlayerActivity.java
+++ app/src/main/java/activity/AudioPlayerActivity.java
@@ -1,28 +1,165 @@
 package activity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.example.wzh.appplayer321.IMusicPlayService;
 import com.example.wzh.appplayer321.R;
 
 import service.MusicPlayService;
 
-public class AudioPlayerActivity extends AppCompatActivity {
-    private ImageView iv_icon;
+import static com.example.wzh.appplayer321.R.id.iv_icon;
+
+public class AudioPlayerActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private RelativeLayout rlTop;
+    private ImageView ivIcon;
+    private TextView tvArtist;
+    private TextView tvAudioname;
+    private LinearLayout llBottom;
+    private TextView tvTime;
+    private SeekBar seekbarAudio;
+    private Button btnPlaymode;
+    private Button btnPre;
+    private Button btnStartPause;
+    private Button btnNext;
+    private Button btnLyric;
+    //这个就是IMusicPlayService.Stub的实例
+    private IMusicPlayService service;
+    private int position;
+
+
+    private ServiceConnection conon = new ServiceConnection() {
+        //绑定服务成功
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder) {
+            service =  IMusicPlayService.Stub.asInterface(iBinder);
+            if(service != null){
+                try {
+                    service.openAudio(position);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //断开链接
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+    private void findViews() {
+
+        setContentView(R.layout.activity_audio_player);
+        ivIcon = (ImageView)findViewById(iv_icon);
+        ivIcon.setBackgroundResource(R.drawable.animation_bg);
+        AnimationDrawable background = (AnimationDrawable) ivIcon.getBackground();
+        background.start();
+        rlTop = (RelativeLayout)findViewById( R.id.rl_top );
+        ivIcon = (ImageView)findViewById( iv_icon );
+        tvArtist = (TextView)findViewById( R.id.tv_artist );
+        tvAudioname = (TextView)findViewById( R.id.tv_audioname );
+        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
+        tvTime = (TextView)findViewById( R.id.tv_time );
+        seekbarAudio = (SeekBar)findViewById( R.id.seekbar_audio );
+        btnPlaymode = (Button)findViewById( R.id.btn_playmode );
+        btnPre = (Button)findViewById( R.id.btn_pre );
+        btnStartPause = (Button)findViewById( R.id.btn_start_pause );
+        btnNext = (Button)findViewById( R.id.btn_next );
+        btnLyric = (Button)findViewById( R.id.btn_lyric );
+
+        btnPlaymode.setOnClickListener( this );
+        btnPre.setOnClickListener( this );
+        btnStartPause.setOnClickListener( this );
+        btnNext.setOnClickListener( this );
+        btnLyric.setOnClickListener( this );
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audio_player);
-        iv_icon = (ImageView)findViewById(R.id.iv_icon);
-        iv_icon.setBackgroundResource(R.drawable.animation_bg);
-        AnimationDrawable background = (AnimationDrawable) iv_icon.getBackground();
-        background.start();
 
+        findViews();
+        getData();
+        startAndBindService();
 
         Intent intent = new Intent(this, MusicPlayService.class);
         startService(intent);
+    }
+
+    private void startAndBindService() {
+        Intent intent = new Intent(this, MusicPlayService.class);
+        //保证只有一个实例
+        bindService(intent,conon, Context.BIND_AUTO_CREATE);
+        startService(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if ( view == btnPlaymode ) {
+            // Handle clicks for btnPlaymode
+        } else if ( view == btnPre ) {
+            // Handle clicks for btnPre
+        } else if ( view == btnStartPause ) {
+            // Handle clicks for btnStartPause
+            try {
+                if(service.isPlaying()){
+                    //暂停
+                    service.pause();
+                    //显示播放按钮
+                    btnStartPause.setBackgroundResource(R.drawable.btn_audio_start_selector);
+                }else{
+                    //播放
+                    service.start();
+                    //显示暂停按钮
+                    btnStartPause.setBackgroundResource(R.drawable.btn_audio_pause_selector);
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+
+        } else if ( view == btnNext ) {
+            // Handle clicks for btnNext
+        } else if ( view == btnLyric ) {
+            // Handle clicks for btnLyric
+        }
+    }
+
+    public void getData() {
+        position = getIntent().getIntExtra("position",0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(conon != null){
+            unbindService(conon);
+            conon = null;
+        }
     }
 }
